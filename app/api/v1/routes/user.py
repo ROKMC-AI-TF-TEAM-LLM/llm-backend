6b7a288741e6ee18cc31855d59bb59bc73fa781a
rude-ore098 <rudeore0928@gmail.com>
2026-05-21 14:50:42 +0900

    [FIX] 스웨거 수정

--- app/api/v1/routes/user.py
+++ app/api/v1/routes/user.py
@@ -13,7 +13,7 @@ from app.services import user_service
 router = APIRouter(prefix="/users", tags=["users"])
 
 
-@router.post("", response_model=ApiResponse[UserResponse], status_code=status.HTTP_201_CREATED)
+@router.post("", response_model=ApiResponse[UserResponse], status_code=status.HTTP_201_CREATED,  summary="TEST API")
 async def create_user(data: UserCreate, db: AsyncSession = Depends(get_db)):
     user = await user_service.create_user(db, data)
     return ApiResponse.ok(UserResponse.model_validate(user), status_code=201)
@@ -24,19 +24,19 @@ async def get_me(current_user: User = Depends(get_current_user)):
     return ApiResponse.ok(UserProfileResponse.model_validate(current_user), status_code=200)
 
 
-@router.get("/{user_id}", response_model=ApiResponse[UserResponse])
+@router.get("/{user_id}", response_model=ApiResponse[UserResponse], summary="TEST API")
 async def get_user(user_id: uuid.UUID, db: AsyncSession = Depends(get_db)):
     user = await user_service.get_user(db, user_id)
     return ApiResponse.ok(UserResponse.model_validate(user), status_code=200)
 
 
-@router.patch("/{user_id}", response_model=ApiResponse[UserResponse])
+@router.patch("/{user_id}", response_model=ApiResponse[UserResponse], summary="TEST API")
 async def update_user(user_id: uuid.UUID, data: UserUpdate, db: AsyncSession = Depends(get_db)):
     user = await user_service.update_user(db, user_id, data)
     return ApiResponse.ok(UserResponse.model_validate(user), status_code=200)
 
 
-@router.delete("/{user_id}", response_model=ApiResponse[None])
+@router.delete("/{user_id}", response_model=ApiResponse[None], summary="TEST API")
 async def delete_user(user_id: uuid.UUID, db: AsyncSession = Depends(get_db)):
     await user_service.delete_user(db, user_id)
     return ApiResponse.ok(status_code=200)

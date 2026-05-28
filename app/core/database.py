a4ced62dc286c892d7b2efcae378665cc3389e5a
rude-ore098 <rudeore0928@gmail.com>
2026-05-28 19:51:33 +0900

    [REFEAT] db ssl 우회

--- app/core/database.py
+++ app/core/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import DeclarativeBase
 
 from app.core.config import settings
 
-engine = create_async_engine(settings.database_url, echo=False)
+engine = create_async_engine(settings.database_url, echo=False, connect_args={"ssl":False})
 
 AsyncSessionLocal = async_sessionmaker(
     engine,

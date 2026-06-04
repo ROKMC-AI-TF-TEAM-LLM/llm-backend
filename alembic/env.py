042779e9dd5ccd744f79e9c5548d34a554b6d2ce
Shuten-doji-ex <newrunone00@gmail.com>
2026-06-04 09:43:31 +0900

    [FIX]alembic 한글 경로 문제 수정

--- alembic/env.py
+++ alembic/env.py
@@ -33,7 +33,8 @@ def do_run_migrations(connection):
 
 
 async def run_migrations_online() -> None:
-    engine = create_async_engine(settings.database_url)
+    # engine = create_async_engine(settings.database_url)
+    engine = create_async_engine(settings.database_url, connect_args={"ssl": False})
     async with engine.connect() as connection:
         await connection.run_sync(do_run_migrations)
     await engine.dispose()
